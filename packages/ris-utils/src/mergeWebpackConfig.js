ff0d1c0741c220a980887fd06d3c186d4a926cc1
guobiao.xgb <guobiao.xgb@alibaba-inc.com>
2019-01-18 16:09:41 +0800

    feat(utils):add merge plugins

--- packages/ris-utils/src/mergeWebpackConfig.js
+++ packages/ris-utils/src/mergeWebpackConfig.js
@@ -1,10 +1,29 @@
 const merge = require('webpack-merge');
 
 module.exports = (clientConfig, defaultConfig) => {
-  const config = merge({}, defaultConfig, clientConfig);
-  // merge rules
-  // we put all user's loaders into oneOf config
-  // we ensure that user's loaders is in the front of our loaders
+  // Merge plugins
+  // There is no problem if client add plugins
+  // If client want to cover the plugins, we remove it from default plugins.
+  const defaults = Object.assign({}, defaultConfig);
+  const defaultPlugins = defaults.plugins || [];
+  const clientPlugins = clientConfig.plugins || [];
+  const clientPluginNames = clientPlugins.map(plugin => plugin.constructor.name);
+
+  const validPlugins = [];
+  defaultPlugins.forEach((item) => {
+    const pluginName = item.constructor.name;
+    if (clientPluginNames.indexOf(pluginName) === -1) {
+      validPlugins.push(item);
+    }
+  });
+
+  defaults.plugins = validPlugins;
+
+  const config = merge({}, defaults, clientConfig);
+
+  // Merge rules
+  // We put all user's loaders into oneOf config
+  // We ensure that user's loaders is in the front of our loaders
   const { rules } = config.module;
   const newRules = [];
   const loaders = [];
@@ -23,5 +42,6 @@ module.exports = (clientConfig, defaultConfig) => {
     }
   });
   config.module.rules = newRules;
+
   return config;
 };
